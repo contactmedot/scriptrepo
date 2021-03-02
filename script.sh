5e8606657a5b54501418787ff57e655c674ebba1
contactmedot <78786765+contactmedot@users.noreply.github.com>
2021-03-02 21:16:59 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -12,3 +12,42 @@ systemctl start docker.service
 echo '-----------------------------------------------------'
 echo 'Hi installing docker Completed from script file'
 echo '-----------------------------------------------------'
+# install node
+echo '-----------------------------------------------------'
+echo 'Hi installing node from script file'
+echo '-----------------------------------------------------'
+yum -y install -y curl
+curl https://rpm.nodesource.com/setup_12.x | bash -
+yum install -y nodejs
+echo '-----------------------------------------------------'
+echo 'Hi node installation completed from script file'
+echo '-----------------------------------------------------'
+# install  git
+echo '-----------------------------------------------------'
+echo 'Hi installing git from script file'
+echo '-----------------------------------------------------'
+yum install -y git
+echo '-----------------------------------------------------'
+echo 'Hi git installation completed from script file'
+echo '-----------------------------------------------------'
+# install  java
+echo '-----------------------------------------------------'
+echo 'Hi installing java from script file'
+echo '-----------------------------------------------------'
+yum install -y java-1.8.0-openjdk
+echo '-----------------------------------------------------'
+echo 'Hi java installation completed from script file'
+echo '-----------------------------------------------------'
+# install maven
+echo '-----------------------------------------------------'
+echo 'Hi installing maven from script file'
+echo '-----------------------------------------------------'
+yum install -y maven
+echo '-----------------------------------------------------'
+echo 'Hi maven installation completed from script file'
+echo '-----------------------------------------------------'
+echo '-----------------------------------------------------'
+echo 'Hi this is  exited execution from script file'
+echo '-----------------------------------------------------'
+echo 'you can check version for  install tool with node -v; npm -version;java -version;docker --version;git --version;mvn -v;'
+echo '-----------------------------------------------------'
